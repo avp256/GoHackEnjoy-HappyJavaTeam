0a930cb8bcbed5b756f4281c98bf647c239d77bc
AVP256 <AVP256COM@GMAIL.COM>
2015-08-02 10:25:39 +0300

    try to do gamer

--- src/main/java/com/codenjoy/dojo/sample/client/ai/FiveTeenSolver.java
+++ src/main/java/com/codenjoy/dojo/sample/client/ai/FiveTeenSolver.java
@@ -8,6 +8,12 @@ import com.codenjoy.dojo.sample.services.GameRunner;
 import com.codenjoy.dojo.services.Dice;
 import com.codenjoy.dojo.services.RandomDice;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Это алгоритм твоего бота. Он будет запускаться в игру с первым
  * зарегистрировавшимся игроком, чтобы ему не было скучно играть самому.
@@ -15,6 +21,18 @@ import com.codenjoy.dojo.services.RandomDice;
  * Для его запуска воспользуйся методом {@see FiveTeenSolver#main}
  */
 public class FiveTeenSolver implements Solver<Board> {
+    private static final char[][] expected =
+            {
+                    {'*', '*', '*', '*', '*', '*'},
+                    {'*', 'a', 'b', 'c', 'd', '*'},
+                    {'*', 'e', 'f', 'g', 'h', '*'},
+                    {'*', 'i', 'j', 'k', 'l', '*'},
+                    {'*', 'm', 'n', 'o', '+', '*'},
+                    {'*', '*', '*', '*', '*', '*'}
+            };
+
+    //private Map<String, char[][]> open;
+    private int size;
 
     public FiveTeenSolver(Dice dice) {
 
@@ -23,8 +41,172 @@ public class FiveTeenSolver implements Solver<Board> {
     @Override
     public String get(final Board board) {
         if (board.isGameOver()) return "";
-        return "UP";
+
+        size = board.size() - 2;
+        char[][] first = getArray(board);
+
+        return calcDirection(first);
     }
+
+    private String calcDirection(char[][] prev) {
+        if (checkDirection(prev, -1, 0)) {
+            return "UP";
+        }
+        if (checkDirection(prev, 1, 0)) {
+            return "DOWN";
+        }
+        if (checkDirection(prev, 0, -1)) {
+            return "LEFT";
+        }
+        if (checkDirection(prev, 0, 1)) {
+            return "RIGHT";
+        }
+        return "";
+    }
+
+    private boolean checkDirection(char[][] prev, int dX, int dY) {
+        ArrayList<char[][]> open = new ArrayList<>();
+        char[][] start = move(prev, dX, dY);
+        if (start == null) {
+            return false;
+        }
+        open.add(prev);
+        open.add(start);
+        while (true) {
+            char[][][] befores = new char[Math.min(4, open.size())][][];
+            if (open.size() - 1 >= 0) {
+                befores[0] = open.get(open.size() - 1);
+            }
+            if (open.size() - 2 >= 0) {
+                befores[1] = open.get(open.size() - 2);
+            }
+            if (open.size() - 3 >= 0) {
+                befores[2] = open.get(open.size() - 3);
+            }
+            if (open.size() - 4 >= 0) {
+                befores[3] = open.get(open.size() - 4);
+            }
+            boolean flag = false;
+            for (char[][] before : befores) {
+                char[][] up = move(before, -1, 0);
+                if (up != null) {
+                    if (equels(up, expected)) {
+                        return true;
+                    }
+                    if (!exist(open, up)) {
+                        flag = true;
+                        open.add(up);
+                    }
+                }
+                char[][] down = move(before, 1, 0);
+                if (down != null) {
+                    if (equels(down, expected)) {
+                        return true;
+                    }
+                    if (!exist(open, down)) {
+                        flag = true;
+                        open.add(down);
+                    }
+                }
+                char[][] left = move(before, 0, -1);
+                if (left != null) {
+                    if (equels(left, expected)) {
+                        return true;
+                    }
+                    if (!exist(open, left)) {
+                        flag = true;
+                        open.add(left);
+                    }
+                }
+                char[][] right = move(before, 0, 1);
+                if (right != null) {
+                    if (equels(right, expected)) {
+                        return true;
+                    }
+                    if (!exist(open, right)) {
+                        flag = true;
+                        open.add(right);
+                    }
+                }
+            }
+            if (!flag) {
+                return false;
+            }
+        }
+    }
+
+    private boolean exist(ArrayList<char[][]> open, char[][] up) {
+        for (char[][] array : open) {
+            if (equels(array, up)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private char[][] move(char[][] prev, int dX, int dY) {
+        char[][] result = new char[size][size];
+
+        int x = -1; //положениие игрока
+        int y = -1; //положениие игрока
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                result[i][j] = prev[i][j]; // копируем в результат
+                if (prev[i][j] == '+') {// вычисляем положениие игрока
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+
+        int newX = x + dX;
+        int newY = y + dY;
+
+        if (newX < 0 || newY < 0 || newX >= size || newY >= size) {
+            return null;
+        }
+
+        char temp = result[x][y];
+        result[x][y] = result[newX][newY];
+        result[newX][newY] = temp;
+
+        return result;
+    }
+
+    private boolean equels(char[][] array1, char[][] array2) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (array1[i][j] != array2[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private char[][] getArray(Board board) {
+        char[][] result = new char[size][size];
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                result[i - 1][j - 1] = board.getAt(j, i).ch();
+            }
+        }
+        return result;
+    }
+
+//    private void initBoard(Board board) {
+//        //open = new LinkedHashMap<>();
+//        size = board.size();
+//
+//        char[][] first = new char[size][size];
+//        for (int i = 0; i < size; i++) {
+//            for (int j = 0; j < size; j++) {
+//                first[i][j] = board.getAt(j, i).ch();
+//            }
+//        }
+//        //open.put(first);
+//    }
 
     /**
      * Метод для запуска игры с текущим ботом. Служит для отладки.
@@ -45,5 +227,4 @@ public class FiveTeenSolver implements Solver<Board> {
             e.printStackTrace();
         }
     }
-
 }
