e0617df1e812d130c0763e6ac508d37bf90fa0c9
AVP256 <AVP256COM@GMAIL.COM>
2015-08-02 14:28:11 +0300

    add direction tests fo solver

--- src/test/java/com/codenjoy/dojo/fifteen/client/SolverTest.java
+++ src/test/java/com/codenjoy/dojo/fifteen/client/SolverTest.java
@@ -2,8 +2,10 @@ package com.codenjoy.dojo.fifteen.client;
 
 import com.codenjoy.dojo.client.Direction;
 import com.codenjoy.dojo.client.Solver;
+import com.codenjoy.dojo.fifteen.client.ai.FifteenSolver;
 import com.codenjoy.dojo.services.Dice;
 import org.junit.Before;
+import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 import static org.mockito.Matchers.anyInt;
@@ -18,54 +20,59 @@ public class SolverTest {
     @Before
     public void setup() {
         dice = mock(Dice.class);
-        ai = new YourSolver(dice);
+        ai = new FifteenSolver(dice);
     }
 
     private Board board(String board) {
         return (Board) new Board().forString(board);
     }
 
-//    @Test
-//    public void should() {
-//        asertAI("☼☼☼☼☼☼☼" +
-//                "☼  x  ☼" +
-//                "☼ $   ☼" +
-//                "☼     ☼" +
-//                "☼ ☺ $ ☼" +
-//                "☼  ☻  ☼" +
-//                "☼☼☼☼☼☼☼", Direction.UP);
-//
-//        asertAI("☼☼☼☼☼☼☼" +
-//                "☼  x  ☼" +
-//                "☼ $   ☼" +
-//                "☼ ☺   ☼" +
-//                "☼   $ ☼" +
-//                "☼  ☻  ☼" +
-//                "☼☼☼☼☼☼☼", Direction.UP);
-//
-//        asertAI("☼☼☼☼☼☼☼" +
-//                "☼  x  ☼" +
-//                "☼ ☺   ☼" +
-//                "☼     ☼" +
-//                "☼   $ ☼" +
-//                "☼$ ☻  ☼" +
-//                "☼☼☼☼☼☼☼", Direction.UP);
-//
-//        asertAI("☼☼☼☼☼☼☼" +
-//                "☼ ☺x  ☼" +
-//                "☼     ☼" +
-//                "☼     ☼" +
-//                "☼   $ ☼" +
-//                "☼$ ☻  ☼" +
-//                "☼☼☼☼☼☼☼", Direction.UP);
-//    }
+    @Test
+    public void downTest() {
+        when(dice.next(anyInt())).thenReturn(0);
+        asertAI("******" +
+                "*abcd*" +
+                "*efgh*" +
+                "*ij+k*" +
+                "*mnol*" +
+                "******", Direction.DOWN);
+    }
+
+    @Test
+    public void upTest() {
+        when(dice.next(anyInt())).thenReturn(1);
+        asertAI("******" +
+                "*abcd*" +
+                "*efgh*" +
+                "*ij+k*" +
+                "*mnol*" +
+                "******", Direction.UP);
+    }
+
+    @Test
+    public void rightTest() {
+        when(dice.next(anyInt())).thenReturn(2);
+        asertAI("******" +
+                "*abcd*" +
+                "*efgh*" +
+                "*ij+k*" +
+                "*mnol*" +
+                "******", Direction.RIGHT);
+    }
+
+    @Test
+    public void leftTest() {
+        when(dice.next(anyInt())).thenReturn(3);
+        asertAI("******" +
+                "*abcd*" +
+                "*efgh*" +
+                "*ij+k*" +
+                "*mnol*" +
+                "******", Direction.LEFT);
+    }
 
     private void asertAI(String board, Direction expected) {
         String actual = ai.get(board(board));
         assertEquals(expected.toString(), actual);
-    }
-
-    private void dice(Direction direction) {
-        when(dice.next(anyInt())).thenReturn(direction.value());
     }
 }
